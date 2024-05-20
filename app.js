eebf5650365948ece4af77685c03e7b440b4b0fa
Rafael Marques <137175315+RafaelMarques-Visi0n4You@users.noreply.github.com>
2024-05-20 19:51:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const faltasRoute = require('./routes/faltasRoute');
+const ideiaRoute = require('./routes/ideiaRoute');
+const feriasRoute = require('./routes/feriasRoute');
+const userRoute = require('./routes/userRoute')
+const calendarioRoute = require('./routes/calendarioRoute');
+const despesasRoute = require('./routes/despesasRoute');
+const blogRoute = require('./routes/blogRoute');
+const noticiaRoute = require('./routes/noticiaRoute');
+const visitaRoute = require('./routes/visitaRoute');
+const candidaturasRoute = require('./routes/candidaturasRoute');
+const departamentoRoute = require('./routes/departamentoRoute');
+const empresaRoute = require('./routes/empresaRoute');
+const notificacoesRoute = require('./routes/notificacoesRoute');
+const projetosRoute = require('./routes/projetosRoute');
+const reembolsosRoute = require('./routes/reembolsosRoute');
+const vagaRoute = require('./routes/vagaRoute');
+const userVisitanteRoute = require('./routes/userVisitanteRoute');
+
+
+app.set('port', process.env.PORT||8080);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type,Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/faltas', faltasRoute);
+app.use('/ideias', ideiaRoute);
+app.use('/ferias', feriasRoute);
+app.use('/user', userRoute);
+app.use('/calendario', calendarioRoute);
+app.use('/despesas', despesasRoute);
+app.use('/blog', blogRoute);
+app.use('/noticia', noticiaRoute);
+app.use('/visita', visitaRoute);
+app.use('/candidaturas', candidaturasRoute);
+app.use('/departamento', departamentoRoute);
+app.use('/empresa', empresaRoute);
+app.use('/notificacoes', notificacoesRoute);
+app.use('/projeto', projetosRoute);
+app.use('/reembolsos', reembolsosRoute);
+app.use('/vaga', vagaRoute);
+app.use('/userVisitante', userVisitanteRoute);
+
+app.listen(app.get('port'), () => {
+    console.log("Start server on port " + app.get('port'));
+})
+

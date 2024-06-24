f1363aeb08f70679f8a03f15613fef9b625640f5
Maria João <96433797+MariaJoao22@users.noreply.github.com>
2024-06-24 15:24:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,19 @@ const corsOptions = {
 app.set('port', process.env.PORT||8080);
 app.use(cors(corsOptions));
 
+app.set('port', process.env.PORT || 10000);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type,Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+app.get('/check', (req, res) => {
+  res.status(200).send({ message: "Wecolme to MHRS API" });
+});
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
